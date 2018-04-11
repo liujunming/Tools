3c59799d2d01092063cb0a2e2801d28c9df6001e
刘军明 <ljm0910@mail.ustc.edu.cn>
2018-04-11 13:36:46 +0800

    Update test.sh

--- RDT/test.sh
+++ RDT/test.sh
@@ -1,9 +1,10 @@
 
 #/bin/bash
 
+# $1为command，如free $2为输出文件名
 result=($($1))
-
 b=$(rm $2)
-
+# https://unix.stackexchange.com/questions/191122/how-to-split-the-string-after-and-before-the-space-in-shell-script
 echo  "time:"${result[9]} >> $2
 echo  "time:"${result[11]}
+
