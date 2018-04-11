b402c102e749f2f8850ca4359b441fa563526d43
刘军明 <ljm0910@mail.ustc.edu.cn>
2018-04-11 13:40:46 +0800

    Update test.sh

--- RDT/test.sh
+++ RDT/test.sh
@@ -1,10 +1,10 @@
 
 #/bin/bash
 
-# $1为command，如free $2为输出文件名
-result=($($1))
-b=$(rm $2)
+result=($(free -m))
+#$1为重定向的文件
+b=$(rm $1)
 # https://unix.stackexchange.com/questions/191122/how-to-split-the-string-after-and-before-the-space-in-shell-script
-echo  "time:"${result[9]} >> $2
+echo  "time:"${result[9]} >> $1
 echo  "time:"${result[11]}
 
