e9f312dbabf3ab02a106bded3f110626b43c3b91
刘军明 <ljm0910@mail.ustc.edu.cn>
2018-10-09 22:13:39 +0800

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,1 +1,39 @@
+#!/usr/bin/env python
+import re
 
+load_count = []
+store_count = []
+with open('1.txt') as f:
+    data = f.readlines()
+index = 1
+for line in data:
+    line_data = line.strip()
+    words = line.split()
+    #print(words[0])
+    if re.match("^\d+?\.\d+?$", words[0]):
+        if index > 500:
+            break
+        #print(words[1])
+        count = int(words[1].replace(',', ''))
+        load_count.append(count)
+        index += 1
+
+with open('2.txt') as f:
+    data = f.readlines()
+index = 1
+for line in data:
+    line_data = line.strip()
+    words = line.split()
+    if re.match("^\d+?\.\d+?$", words[0]):
+        if index > 500:
+            break
+        count = int(words[1].replace(',', ''))
+        store_count.append(count)
+        index += 1
+result = []
+index = 0
+while index < 500:
+    result.append(load_count[index] + store_count[index])
+    index += 1
+
+print(result)
