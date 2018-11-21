fd7258b13586a5da728e09b66185f72265362c0e
刘军明 <ljm0910@mail.ustc.edu.cn>
2018-11-21 12:29:59 +0800

    Add files via upload

---
+++ 虚拟机实验/sharing_stat.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+a=0
+b=$(rm $1)
+while :
+do
+    a=$(($a+2))
+    sharing=$(cat /sys/kernel/mm/ksm/pages_sharing)
+ 
+    echo "time: "$a >> $1
+    echo "page sharing: "$sharing >> $1
+
+
+    echo "time: "$a"s"
+    echo "page sharing: "$sharing 
+
+    sleep 2
+   
+done
